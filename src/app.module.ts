816a728d4aa37cb0429d3be3ca84b2544e443bdd
LAPTOP-TPFDEV-G\tpf-dev <dev@tpf.or.kr>
2025-07-25 11:16:32 +0900

    TypeORM 과 DB 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './res/user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        retryAttempts: 10,
+        type: process.env.DB_TYPE as 'mysql' | 'mariadb',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
+        database: process.env.DB_DATABASE,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        entities: [path.join(__dirname, '/entities/**/*.entity.{js, ts}')],
+        synchronize: false,
+        logging: true,
+        timezone: 'local',
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
