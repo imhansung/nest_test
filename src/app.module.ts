feb29f8e45e759591b1073a2070ad9f37da755fe
LAPTOP-TPFDEV-G\tpf-dev <dev@tpf.or.kr>
2025-07-25 14:09:44 +0900

    #6. 기초부터 따라하는 Nest.js : dotenv와 ConfigModule을 이용한 환경변수 관리

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './res/user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as path from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['.env.development.local'],
+      isGlobal: true,
+      // envFilePath: ['.env.development.local'],
     }),
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        retryAttempts: 10,
-        type: process.env.DB_TYPE as 'mysql' | 'mariadb',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
-        database: process.env.DB_DATABASE,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        retryAttempts: configService.get('NODE_ENV') === 'prod' ? 10 : 1,
+        type: 'mariadb',
+        host: configService.get('DB_HOST'),
+        port: Number(configService.get('DB_PORT')),
+        database: String(configService.get('DB_NAME')),
+        username: configService.get('DB_USER'),
+        password: String(configService.get('DB_PASSWORD')),
         entities: [path.join(__dirname, '/entities/**/*.entity.{js, ts}')],
         synchronize: false,
         logging: true,
