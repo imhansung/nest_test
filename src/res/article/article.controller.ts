32c7fd829d2115bfc0f21015ececd5097c08cf0b
LAPTOP-TPFDEV-G\tpf-dev <dev@tpf.or.kr>
2025-09-09 11:18:20 +0900

    #11. 기초부터 따라하는 Nest.js : Article CRUD - 2  - readArticle
    https://suloth.tistory.com/92

--- src/res/article/article.controller.ts
+++ src/res/article/article.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
 import { User } from 'src/decorators/user.decorator';
@@ -20,6 +20,15 @@ export class ArticleController {
       content,
       userId,
     );
+    return article;
+  }
+
+  @Get('/:id')
+  async readArticle(@Param('id') id) {
+    const articleId = id;
+    
+    const article = await this.articleService.getArticle(articleId);
+    
     return article;
   }
 }
--- src/res/article/article.service.ts
+++ src/res/article/article.service.ts
@@ -18,4 +18,14 @@ export class ArticleService {
     });
     return article;
   }
+
+  async getArticle(articleId: string) {
+    const article = await this.articleRepository.findOne({
+      where: {
+        id: articleId,
+      },
+    });
+
+    return article;
+  }
 }
